825f770465f16928f2dd23a74a131c3d225fb0d2
keshavpudasaini <144650034+keshavpudasaini@users.noreply.github.com>
2023-09-11 10:52:11 +0545

    Add files via upload

---
+++ new.script.R
@@ -0,0 +1,15 @@
+num1 <- 10
+num2 <- 5
+result <- num1 + num2
+result
+
+
+
+# This is a simple "Hello, World!" example in R.
+print("Hello, World!")
+
+
+num3 <- 56789567
+num4 <- 7809876
+result <- num3 + num4
+result
